59cb67878e58c4db2fa266faf485140e9f6ea020
Ritik Rana <rhitikmagarrana111@gmail.com>
2026-09-24 16:37:52 +0545

    connect frontend with backend

--- backend/src/app.js
+++ backend/src/app.js
@@ -11,7 +11,7 @@ const app = express()
 
 
 app.use(cors({
-  origin: "https://portfolio-six-pi-zi8hfehqi0.vercel.app/",
+  origin: "https://portfolio-six-pi-zi8hfehqi0.vercel.app",
   credentials: true
 }));
 app.use(express.json())
